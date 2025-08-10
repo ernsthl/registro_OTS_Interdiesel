9c283dbcaa49c9a33395e9c3f33c8102baa02759
ernsthl <ernsthl@outlook.com>
2025-08-09 21:20:45 -0500

    Update database_mysql.py

--- database_mysql.py
+++ database_mysql.py
@@ -111,8 +111,8 @@ def obtener_orden_por_numero(ot_numero):
         conn = conectar()
         cursor = conn.cursor(dictionary=True)  # Retorna filas como diccionario
 
-        sql = "SELECT * FROM orden_trabajo WHERE ot_numero = %s"
-        cursor.execute(sql, (ot_numero,))
+        sql = "SELECT * FROM orden_trabajo WHERE numero_ot = %s"
+        cursor.execute(sql, (numero_ot,))
         resultado = cursor.fetchone()  # Solo una fila
 
         return resultado  # Diccionario o None si no encontró
