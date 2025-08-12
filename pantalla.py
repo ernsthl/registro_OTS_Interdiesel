c08326ae135025da3b0317a8ad93e64c1ebb8837
ernsthl <ernsthl@outlook.com>
2025-08-11 21:23:07 -0500

    Update pantalla.py

--- pantalla.py
+++ pantalla.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Pantalla de Producción", layout="wide")
 # Logo + Título en la misma fila
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image("Logo_interdiesel.jpg", use_container_width=True)
+    st.image("Logo_interdiesel.jpg", use_column_width=True)
 with col2:
     st.markdown("""
     <h2 style='margin:0; padding-top:10px;'>
@@ -109,3 +109,4 @@ else:
     styled_df = df.style.apply(color_fila, axis=1).set_table_styles(table_styles)
     html = styled_df.to_html()
     st.markdown(html, unsafe_allow_html=True)
+
