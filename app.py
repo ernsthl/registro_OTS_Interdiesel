b0b803b2e6090ddbe994f33327daee02b5063d39
ernsthl <ernsthl@outlook.com>
2026-08-13 22:31:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ def actualizar_last_update_json():
 # -------------------- Configuración inicial --------------------
 st.set_page_config(page_title="Registro de OTs", layout="wide")
 st.image("Logo_interdiesel.jpg", width=400)
-crear_tablas()
 
 st.markdown("## 🧾 Sistema de Control de Órdenes de Trabajo")
 st.markdown("---")
